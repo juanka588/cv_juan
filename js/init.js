c7b5ad43a4e1c958e9b091bd1f7c6973b9ecc022
juanka588 <jcrodriguezd@unal.edu.co>
2017-02-20 17:10:45 -0500

    try to fusion

--- js/init.js
+++ js/init.js
@@ -1,47 +1,28 @@
-strings = {};
-language = "en";
+var firebaseApp = initFireBase();
+var angularApp = angular.module('cvApp', []);
+
+var strings = {};
+var language = "en";
 
 (function ($) {
     $(function () {
-        // Initialize Firebase
-        var config = {
-            apiKey: "AIzaSyAaoOSHTD7s_TVjhHRIZYvSvwD27W-Vy6Q",
-            authDomain: "cvitae-dad25.firebaseapp.com",
-            databaseURL: "https://cvitae-dad25.firebaseio.com",
-            storageBucket: "cvitae-dad25.appspot.com",
-            messagingSenderId: "735079542401"
-        };
-        firebase.initializeApp(config);
-        $('.button-collapse').sideNav({
-            menuWidth: 300, // Default is 240
-            edge: 'left', // Choose the horizontal origin
-            closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-        }
-        );
-        var nameRef = firebase.database().ref('contact');
-        nameRef.on('value', function (snapshot) {
-            console.log("k:" + snapshot.key + " v:" + snapshot.val().name);
-        });
-        firebase.database().ref('strings').once('value').then(function (snapshot) {
-            console.log(snapshot.val());
-            snapshot.forEach(function (child) {
-                strings[child.key] = child.val();
-            });
-
-            loadStrings();
-        });
-
+        $(".dropdown-button").dropdown();
+        initMain();
     }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-
-function loadStrings() {
-    for (var index in strings) {
-        console.log("new values " + strings[index][language]);
-    }
+function initFireBase() {
+// Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAaoOSHTD7s_TVjhHRIZYvSvwD27W-Vy6Q",
+        authDomain: "cvitae-dad25.firebaseapp.com",
+        databaseURL: "https://cvitae-dad25.firebaseio.com",
+        storageBucket: "cvitae-dad25.appspot.com",
+        messagingSenderId: "735079542401"
+    };
+    return firebase.initializeApp(config);
 }
 
-var angularApp = angular.module('cvApp', []);
 
 angularApp.controller('languagesController', function () {
     this.languages = ['en', 'es', 'fr'];
@@ -53,5 +34,39 @@ angularApp.controller('languagesController', function () {
 });
 
 angularApp.controller('cvController', function () {
-    this.test = [5, 3, 4];
+    var controller=this;
+    var nameRef = firebaseApp.database().ref('contact');
+    controller.test = [];
+    nameRef.on('value', function (snapshot) {
+        console.log(snapshot.key);
+        console.log(snapshot.val().name);
+        controller.test['contact'] = snapshot.val();
+    });
 });
+
+
+function initMain() {
+    $('.button-collapse').sideNav({
+        menuWidth: 300, // Default is 240
+        edge: 'left', // Choose the horizontal origin
+        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+    );
+
+    firebaseApp.database().ref('strings').once('value').then(function (snapshot) {
+        console.log(snapshot.val());
+        snapshot.forEach(function (child) {
+            strings[child.key] = child.val();
+        });
+
+        loadStrings();
+    });
+}
+
+
+function loadStrings() {
+    for (var index in strings) {
+        console.log("new values " + strings[index][language]);
+    }
+}
+
