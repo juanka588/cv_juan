6ba9c880ee9b644d01cb56bd638e79a757f93a1c
juanka588 <jcrodriguezd@unal.edu.co>
2017-03-17 16:35:20 -0500

    add other infos

--- js/init.js
+++ js/init.js
@@ -6,6 +6,7 @@ var language = "en";
 
 (function ($) {
     $(function () {
+        initMain();
         $("#printButton").click(function () {
             var DocumentContainer = document.getElementById('section-to-print');
             var WindowObject = window.open('', 'PrintWindow', 'width=750,height=650,top=50,left=50,toolbars=no,scrollbars=yes,status=no,resizable=yes');
@@ -24,7 +25,6 @@ var language = "en";
             WindowObject.print();
             // WindowObject.close();
         });
-        initMain();
     }); // end of document ready
 })(jQuery); // end of jQuery name space
 
@@ -41,31 +41,37 @@ function initFireBase() {
 }
 
 
-angularApp.controller('languagesController', function () {
+var lController = angularApp.controller('languagesController', function () {
     this.languages = ['en', 'es', 'fr'];
     this.updateLanguage = function (lang) {
         language = lang;
-        loadStrings();
     };
 });
 
 angularApp.controller('cvController', function ($firebaseObject) {
+    var controller = this;
     var nameRef = firebaseApp.database().ref('contact');
     var langsRef = firebaseApp.database().ref('languages');
     var educationRef = firebaseApp.database().ref('education').orderByChild('period_finish');
-    educationRef.on('value', function (snap) {
-        console.log(snap.val());
-    });
-
     var workRef = firebaseApp.database().ref('work_experience').orderByChild('period_finish');
     var programmingRef = firebaseApp.database().ref('programming_experience');
-    this.contact = $firebaseObject(nameRef);
-    this.languages = $firebaseObject(langsRef);
-    this.education = $firebaseObject(educationRef);
-    this.work = $firebaseObject(workRef);
-    this.programming = $firebaseObject(programmingRef);
-    console.log(this.programming);
-    this.getName = function (key) {
+    var networkRef = firebaseApp.database().ref('network_experience');
+    var toolsRef = firebaseApp.database().ref('programming_tools');
+    var projectsRef = firebaseApp.database().ref('projects');
+
+    controller.contact = $firebaseObject(nameRef);
+    controller.languages = $firebaseObject(langsRef);
+    controller.education = $firebaseObject(educationRef);
+    controller.work = $firebaseObject(workRef);
+    controller.programming = $firebaseObject(programmingRef);
+    controller.network = $firebaseObject(networkRef);
+    controller.tools = $firebaseObject(toolsRef);
+    controller.projects = $firebaseObject(projectsRef);
+
+    controller.getName = function (key) {
+        if (!strings[key]) {
+            return "cargando";
+        }
         return strings[key][language];
     };
 });
@@ -84,15 +90,6 @@ function initMain() {
         snapshot.forEach(function (child) {
             strings[child.key] = child.val();
         });
-
-        loadStrings();
     });
-}
-
-
-function loadStrings() {
-    for (var index in strings) {
-        //console.log("new values " + strings[index][language]);
-    }
 }
 
