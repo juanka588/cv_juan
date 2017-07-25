43eed06e916753add74e49f809c26a3036d67463
juanka588 <juanka588@gmail.com>
2017-07-25 16:48:02 -0500

    add contests

--- js/cvController.js
+++ js/cvController.js
@@ -5,12 +5,14 @@ angularApp.controller('cvController', function ($firebaseObject) {
     var programmingRef = firebaseApp.database().ref('programming_experience');
     var networkRef = firebaseApp.database().ref('network_experience');
     var toolsRef = firebaseApp.database().ref('programming_tools');
+    var contestsRef = firebaseApp.database().ref('contests');
 
     controller.languages = $firebaseObject(langsRef);
     controller.education = $firebaseObject(educationRef);
     controller.programming = $firebaseObject(programmingRef);
     controller.network = $firebaseObject(networkRef);
     controller.tools = $firebaseObject(toolsRef);
+    controller.contests = $firebaseObject(contestsRef);
 
     controller.getName = function (key) {
         return getName(key);
