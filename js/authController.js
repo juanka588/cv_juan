b2bee4fccc98e62637bc1f0e435253a0ab2dca5f
juanka588 <jcrodriguezd@unal.edu.co>
2017-08-01 20:34:04 -0500

    add auth controller fix char for income and outcome

---
+++ js/authController.js
@@ -0,0 +1,27 @@
+angularApp.controller('authController', function ($firebaseAuth) {
+    this.authObj = $firebaseAuth();
+    this.showUser = false;
+    // any time auth state changes, add the user data to scope
+    this.authObj.$onAuthStateChanged(function (firebaseUser) {
+        this.firebaseUser = firebaseUser;
+        if (this.firebaseUser) {
+            this.showUser = true;
+        }
+        console.log(this.firebaseUser);
+    });
+
+    this.signIn = function () {
+        this.authObj.$signInWithPopup("facebook").then(function (result) {
+            console.log("Signed in as:", result.user);
+        }).catch(function (error) {
+            console.error("Authentication failed:", error);
+        });
+        this.firebaseUser = this.authObj.$getAuth();
+    };
+
+    this.signOut = function () {
+        this.authObj.$signOut();
+    };
+});
+
+
