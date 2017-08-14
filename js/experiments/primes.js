014882e2a4cc01b5155d905a90da25b6181bf8c9
juanka588 <juanka588@gmail.com>
2017-08-14 14:34:14 -0500

    add controls for primes experiment

--- js/experiments/primes.js
+++ js/experiments/primes.js
@@ -3,21 +3,55 @@ angularApp.controller('primesController', function () {
     this.primes = calcPrimes(this.total);
     this.prev = this.primes.length - 1;
     this.current = 0;
+    this.iterations = 100;
     this.next = 1;
+    this.increasing = false;
+    this.len = 1;
+    this.customNumber = -1;
+
+    this.nextNumber = function () {
+        this.prev = this.current;
+        this.current++;
+        this.next = this.current + 1;
+        this.validateRange();
+    };
+    this.prevNumber = function () {
+        this.next = this.current;
+        this.current--;
+        this.prev = this.current - 1;
+        this.validateRange();
+    };
+
+    this.validateRange = function () {
+        if (this.current < 0) {
+            this.current = this.total - 1;
+        }
+        if (this.prev < 0) {
+            this.prev = this.total - 1;
+        }
+        if (this.next < 0) {
+            this.next = this.total - 1;
+        }
+        this.current = this.current % this.total;
+        this.next = this.next % this.total;
+        this.prev = this.prev % this.total;
+    };
+
     this.run = function () {
-        run(this.primes);
+        run(this);
     };
 });
 
-function run(primes) {
+function run(controller) {
 
     new p5(function (s) {
-        var maxIter = 100;
+        var primes = controller.primes;
+        var maxIter = controller.iterations;
         var counter = 0;
         var iteration = 0;
         var rotation = 1;
-        var p = p = primes[iteration];
-        var len = 1;
+        var p = primes[iteration];
+        var len = controller.len;
         var movements = [];
 
         s.setup = function () {
@@ -35,7 +69,11 @@ function run(primes) {
             s.stroke(255);
             s.translate(s.width / 2, s.height / 2);
             rotation = (rotation * p) % 360;
-            len++;
+            if (controller.increasing) {
+                len++;
+            } else {
+                len = 30;
+            }
             movements.push({
                 rotation: rotation,
                 dist: len
@@ -47,20 +85,26 @@ function run(primes) {
                 s.translate(0, m.dist);
             }
             if (counter > maxIter) {
-                counter = 0;
-                rotation = 1;
-                movements = [];
-                len = 1;
-                iteration++;
-                p = primes[iteration];
+                nextNumber();
             }
             counter++;
             s.pop();
         };
 
+        function nextNumber() {
+            maxIter = controller.iterations;
+            counter = 0;
+            rotation = 1;
+            movements = [];
+            len = controller.len;
+            iteration = controller.current;
+            p = primes[iteration];
+        }
+
 
     }, "sketch-holder");
 }
+
 
 function calcPrimes(size) {
 //                0       1      2     3
